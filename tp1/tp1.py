0221007ca6e51e1ee200187a1e734991e19d648a
guido <guido.molaro@ing.austral.edu.ar>
2022-08-13 14:03:35 -0300

    tp1

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -5,4 +5,20 @@ from gitFabri.tp_deteccion.frame_editor import apply_color_convertion, adaptive_
 from gitFabri.tp_deteccion.trackbar import create_trackbar, get_trackbar_value
 
 def main():
-    win
+
+    window_name = 'Window'
+    trackbar_name = 'Trackbar'
+    slider_max = 151
+    cv2.namedWindow(window_name)
+    cap = cv2.VideoCapture(0)
+    biggest_contour = None
+    color_white = (255, 255, 255)
+    create_trackbar(trackbar_name, window_name, slider_max)
+    # saved_hu_moments = load_hu_moments(file_name="hu_moments.txt")
+    saved_contours = []
+
+    while True:
+        ret, frame = cap.read()
+        grayF = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        thresh = cv2.adaptiveThreshold(grayF, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        cv2.imshow('thresh', thresh)
