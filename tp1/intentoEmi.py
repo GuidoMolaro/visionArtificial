9e5d74af9fd91149656c8397509cd3e31a1e0d94
guido <guido.molaro@ing.austral.edu.ar>
2022-08-21 18:27:01 -0300

    intentoEmi update

--- tp1/intentoEmi.py
+++ tp1/intentoEmi.py
@@ -9,18 +9,22 @@ from PIL import Image
 import cv2 as cv
 import numpy as np
 
-webcam = cv.VideoCapture(1)
+webcam = cv.VideoCapture(0)
 
 
 def setBinary(image, val):
     gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
-    ret1, thresh1 = cv.threshold(gray, val, 255,cv.THRESH_BINARY_INV)  # aplica funcion threadhole / ret1 si es true --> significa q no tenemos error
+    ret1, thresh1 = cv.threshold(gray, val, 255,
+                                 cv.THRESH_BINARY_INV)  # aplica funcion threadhole / ret1 si es true --> significa q no tenemos error
     return thresh1
+
 
 def setBinaryAutom(image):
     gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
-    ret1, thresh1 = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)  # aplica funcion threadhole / ret1 si es true --> significa q no tenemos error
+    ret1, thresh1 = cv.threshold(gray, 0, 255,
+                                 cv.THRESH_BINARY + cv.THRESH_OTSU)  # aplica funcion threadhole / ret1 si es true --> significa q no tenemos error
     return thresh1
+
 
 def dilatation(img):
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
@@ -41,16 +45,16 @@ def denoise(img, val1, val2):
 def getContours(binary, img):
     contours, hierarchy = cv.findContours(binary, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
-    # cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+    cv.drawContours(img, contours, -1, (0, 0, 255), 3)
     # cv.imshow("Rayo", img)
     for i in contours:
         area = cv.contourArea(i)
         if area > 1000:
-            cv.drawContours(img, i, -1, (255, 255, 255), 7)
             peri = cv.arcLength(i, True)
             approx = cv.approxPolyDP(i, 0.02 * peri, True)
     cv.imshow("Contours", img)
     return contours
+
 
 def getBiggestContour(contours):
     try:
@@ -61,9 +65,10 @@ def getBiggestContour(contours):
         return max_cnt
     except:
         return contours
-def imagesContours(): #devuelve un array con todos los contornos de las img
+
+
+def imagesContours():  # devuelve un array con todos los contornos de las img
     circulo = setBinaryAutom(np.array(Image.open('circulo.png')))
-    #circulo = setBinaryAutom('tp1/circulo.png')
     triangulo = setBinaryAutom(np.array(Image.open('triangulo.png')))
     rectangulo = setBinaryAutom(np.array(Image.open('rectangulo.png')))
 
@@ -78,16 +83,18 @@ def imagesContours(): #devuelve un array con todos los contornos de las img
     }
     return contours
 
+
 def match(contour):
     moments = cv.moments(contour)
     huMoments = cv.HuMoments(moments)
 
     contours = imagesContours()
     for i in contours.keys():
-        distance = cv.matchShapes(contour,contours[i],cv.CONTOURS_MATCH_I2,0)
-        if distance < 0.3:
-            return i
-    return "null"
+        distance = cv.matchShapes(contour, contours[i], cv.CONTOURS_MATCH_I2, 0)
+        print(distance)
+        if (distance < 100):
+            return True
+    return False
 
 
 def main():
@@ -98,7 +105,7 @@ def main():
     while True:
         tecla = cv.waitKey(30)
         ret, img = webcam.read()
-        cv.imshow('webcam', img)
+        # cv.imshow('webcam', img)
 
         val = cv.getTrackbarPos("Thresh", "binary")
 
@@ -111,12 +118,12 @@ def main():
 
         contours = getContours(denoisedImg, img)
 
-        if match(getBiggestContour(contours)) == "null":
-            for i in contours:
-                cv.drawContours(img, i, -1, (0, 0, 255), 7)
-        else:
+        if match(getBiggestContour(contours)):
             for i in contours:
                 cv.drawContours(img, i, -1, (0, 255, 0), 7)
+        else:
+            for i in contours:
+                cv.drawContours(img, i, -1, (0, 0, 255), 7)
         if tecla == 27:
             break
 
