b7dde0221028137ae3338e43bd9a8fc65470cd93
CamilaContestabile <camila.contestabile@ing.austral.edu.ar>
2022-08-30 10:55:21 -0300

    resuelto el tema del excel, log e inf predictor tira error rectangulo ahora es estrella

--- tp2/DescriptorGenerator.py
+++ tp2/DescriptorGenerator.py
@@ -1,11 +1,12 @@
 import cv2 as cv
+import numpy as np
 import pandas as pd
 import openpyxl as openpyxl
 def descriptionGenerator(filename):
     im = cv.imread(filename, cv.IMREAD_GRAYSCALE)
     _, im = cv.threshold(im, 128, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
     moment = cv.moments(im)
-    huMoment = cv.HuMoments(moment).flatten()
+    huMoment = cv.HuMoments(moment)
     return huMoment
 
 def dataset(info):
@@ -16,43 +17,72 @@ def dataset(info):
         descriptionGenerator('10triangles/t10.jpeg'),
         descriptionGenerator('10circles/c1.png'),
         descriptionGenerator('10circles/c2.png'),
-        descriptionGenerator('10rectangles/r1.png'),
-        descriptionGenerator('10rectangles/r2.png'),
+        descriptionGenerator('10stars/e1.png'),
+        descriptionGenerator('10stars/e2.png'),
         descriptionGenerator('10triangles/t5.jpeg'),
-        descriptionGenerator('10rectangles/r3.png'),
+        descriptionGenerator('10stars/e3.png'),
         descriptionGenerator('10circles/c3.png'),
-        descriptionGenerator('10rectangles/r5.png'),
-        descriptionGenerator('10rectangles/r6.png'),
+        descriptionGenerator('10stars/e5.jpg'),
+        descriptionGenerator('10stars/e6.png'),
         descriptionGenerator('10circles/c4.png'),
         descriptionGenerator('10circles/c5.png'),
         descriptionGenerator('10circles/c6.png'),
-        descriptionGenerator('10rectangles/r7.png'),
-        descriptionGenerator('10rectangles/r8.png'),
+        descriptionGenerator('10stars/e7.jpg'),
+        descriptionGenerator('10stars/e8.png'),
         descriptionGenerator('10triangles/t1.jpeg'),
         descriptionGenerator('10triangles/t2.jpeg'),
         descriptionGenerator('10triangles/t3.jpeg'),
         descriptionGenerator('10triangles/t4.jpeg'),
-        descriptionGenerator('10rectangles/r9.jpg'),
-        descriptionGenerator('10rectangles/r10.png'),
+        descriptionGenerator('10stars/e9.jpg'),
+        descriptionGenerator('10stars/e10.png'),
         descriptionGenerator('10circles/c7.png'),
         descriptionGenerator('10circles/c8.png'),
-        descriptionGenerator('10rectangles/r4.png'),
+        descriptionGenerator('10stars/e4.png'),
         descriptionGenerator('10circles/c9.png'),
         descriptionGenerator('10triangles/t6.jpeg'),
         descriptionGenerator('10triangles/t7.jpeg'),
-        descriptionGenerator('10circles/c10.png'),
-    ]
+        descriptionGenerator('10circles/c10.png')]
     tags = [3, 3, 3, 1, 1, 2, 2, 3, 2, 1, 2, 2, 1, 1, 1, 2, 2, 3, 3, 3, 3, 2, 2, 1, 1, 2, 1, 3, 3, 1]
+
+    rows = 30
+    columns = 7
+    for i in range(rows):
+        for j in range(columns):
+            dataset[i][j] = -1 * np.copysign(1.0, dataset[i][j]) * np.log10(
+                np.absolute(dataset[i][j]))
+
     if info ==1:
         return dataset
     if info==0:
         return tags
+
+def createArray (huNumber, data):
+    hu = [
+        data[i][huNumber-1]
+        for i in range(30)
+    ]
+    return hu
+
 def main():
     data = dataset(1)
     tags = dataset(0)
     col1 = "tags"
-    col2 = "huInvariants"
-    data = pd.DataFrame({col1: tags, col2: data})
+    col2 = "hu1"
+    col3 = "hu2"
+    col4 = "hu3"
+    col5 = "hu4"
+    col6 = "hu5"
+    col7 = "hu6"
+    col8 = "hu7"
+    hu1 = createArray(1, data)
+    hu2 = createArray(2, data)
+    hu3 = createArray(3, data)
+    hu4 = createArray(4, data)
+    hu5 = createArray(5, data)
+    hu6 = createArray(6, data)
+    hu7 = createArray(7, data)
+
+    data = pd.DataFrame({col1: tags, col2: hu1, col3:hu2, col4: hu3, col5: hu4, col6:hu5, col7:hu6, col8:hu7})
     data.to_excel('data.xlsx', sheet_name='sheet1', index=False)
 
 main()
--- tp2/Predictor.py
+++ tp2/Predictor.py
@@ -1,9 +1,7 @@
 from math import copysign, log10
 
-from PIL import Image
 import cv2 as cv
-import numpy as np
-from joblib import dump, load
+from joblib import load
 
 webcam = cv.VideoCapture(1)
 classifier = load('classifier.joblib')
@@ -13,28 +11,18 @@ def setBinary(image, val):
     ret1, thresh1 = cv.threshold(gray, val, 255, cv.THRESH_BINARY_INV)
     return thresh1
 
-
-def setBinaryAutom(image):
-    gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
-    ret1, thresh1 = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-    return thresh1
-
-
 def dilatation(img):
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
     return cv.dilate(img, kernel, iterations=1)
-
 
 def erosion(img):
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
     return cv.erode(img, kernel, iterations=1)
 
-
 def denoise(img, val1, val2):
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (val1 + 1, val2 + 1))
     tempImg = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
     return cv.morphologyEx(tempImg, cv.MORPH_CLOSE, kernel)
-
 
 def getContours(binary, img):
     contours, hierarchy = cv.findContours(binary, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
@@ -46,35 +34,6 @@ def getContours(binary, img):
             peri = cv.arcLength(i, True)
             approx = cv.approxPolyDP(i, 0.02 * peri, True)
             cv.drawContours(img, contours, -1, (255, 0, 0), 3)
-    return contours
-
-
-def getBiggestContour(contours):
-    try:
-        max_cnt = contours[0]
-        for cnt in contours:
-            if cv.contourArea(cnt) > cv.contourArea(max_cnt):
-                max_cnt = cnt
-        return max_cnt
-    except:
-        return contours
-
-
-def imagesContours():  # devuelve un array con todos los contornos de las img
-    circulo = setBinaryAutom(np.array(Image.open('circulo.png')))
-    triangulo = setBinaryAutom(np.array(Image.open('triangulo.png')))
-    rectangulo = setBinaryAutom(np.array(Image.open('rectangulo.png')))
-
-
-    circuloContour, hierarchy = cv.findContours(circulo, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    trianguloContour, hierarchy = cv.findContours(triangulo, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    rectanguloContour, hierarchy = cv.findContours(rectangulo, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-
-    contours = {
-        "circulo": getBiggestContour(circuloContour),
-        "rectangulo": getBiggestContour(rectanguloContour),
-        "triangulo": getBiggestContour(trianguloContour)
-    }
     return contours
 
 def lam(x):
@@ -113,6 +72,5 @@ def main():
         cv.imshow('webcam', img)
         if tecla == 27:
             break
-
 
 main()
